184a262675bdcd47d11537de3b47df2f837efeba
KuEmTrinh <vodaitrinh010696@gmail.com>
2022-02-22 16:11:38 +0900

    add Bai 8

--- Buoi8/Bai2.js
+++ Buoi8/Bai2.js
@@ -1,5 +1,9 @@
 // Viết chương trình in ra các số từ 1 đến 10
 
-for(let i = 1; i <= 10; i ++){
-    console.log(i + " ")
+function printFromTo(a, b) {
+  for (a; a <= b; a++) {
+    console.log(a + " ");
+  }
 }
+
+printFromTo(1, 10);
