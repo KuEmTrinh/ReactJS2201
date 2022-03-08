b904867acca0f9544d8d50cbaea085834f85c7ce
KuEmTrinh <vodaitrinh010696@gmail.com>
2022-03-08 23:27:35 +0900

    addung

--- Buoi11/Bai33.js
+++ Buoi11/Bai33.js
@@ -13,3 +13,5 @@ const sortArray = (array) => {
 
 let a = [9, 8, 7, 6, 4, 5, 3, 2, 1];
 console.log(sortArray(a));
+// Để thay đổi một property của một object thì phải deep copy
+// Tức là tạo ra một biến mới xong rồi convert nó về String rồi convert ngược lại JSON
